92975d7fa1de67817f043dc387fa0ebb07e00e1d
saru <sarguments@gmail.com>
2018-09-27 23:45:01 +0900

    add prevStatus, curStatus in Cell Object

--- src/main/java/lifegame/model/Cell.java
+++ src/main/java/lifegame/model/Cell.java
@@ -1,12 +1,23 @@
 package lifegame.model;
 
 public class Cell {
-    public CellStatus evolve() {
+    private CellStatus prevStatus;
+    private CellStatus curStatus;
 
+    public Cell() {
+        prevStatus = CellStatus.DEAD;
+        curStatus = CellStatus.DEAD;
+    }
+
+    public CellStatus evolve() {
         return getCurStatus();
     }
 
     private CellStatus getCurStatus() {
-        return CellStatus.DEAD;
+        return curStatus;
+    }
+
+    public CellStatus getPrevStatus() {
+        return prevStatus;
     }
 }
